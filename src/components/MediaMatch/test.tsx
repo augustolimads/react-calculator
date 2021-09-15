6f1dc08c0cd57c4c194b7fa6960d1d84966f48e6
Augusto Lima <augustolimads@gmail.com>
2021-09-15 13:35:33 -0300

    add mediaMatch component utility

---
+++ src/components/MediaMatch/test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+
+import { MediaMatch } from '.'
+
+describe('<MediaMatch />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<MediaMatch />)
+    expect(screen.getByRole('heading', { name: /MediaMatch/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
